54ef56f39a951a520ab658bfbf9ab237bfb00d83
Amartya Yadav <amartyadav@live.co.uk>
2022-07-06 17:19:42 +0100

    Signin frontend done. signup redirects to signin

--- frontend/src/Components/Signin.js
+++ frontend/src/Components/Signin.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import TextField from '@mui/material/TextField'
+import {Grid} from '@mui/material'
+import Button from '@mui/material/Button'
 
 export default function Signin() {
-  return (
-    <div>
-        <h1>
-            Sign In to Morra
-        </h1>
-    </div>
-  )
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+
+    return (
+        <div>
+            <h1>
+                Sign In to Morra
+            </h1>
+
+            <Grid container
+                spacing={3}>
+                <Grid item
+                    xs={12}>
+                    <TextField id="outlined-basic" label="Email" variant="outlined"
+                        value={email}
+                        onChange={handleEmailChange}/>
+                </Grid>
+                <Grid item
+                    xs={12}>
+                    <TextField id="outlined-basic" label="Password" variant="outlined"
+                        value={password}
+                        onChange={handlePasswordChange}/>
+                </Grid>
+                <Grid item
+                    xs={12}>
+                    <Button variant="contained" color="primary">
+                        Sign In
+                    </Button>
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -2,8 +2,10 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 import {Grid} from '@mui/material'
 import Button from '@mui/material/Button'
+import {useNavigate} from 'react-router-dom'
 
 export default function Signup() {
+    const navigate = useNavigate()
     const [name, setName] = React.useState('')
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
@@ -17,6 +19,27 @@ export default function Signup() {
     const handlePasswordChange = (event) => {
         setPassword(event.target.value);
     };
+    const handleClick = (event) => {
+        console.log("Signup: handleClick");
+        //event.preventDefault();
+        navigate('/signin');
+        // fetch('/api/users', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         name,
+        //         email,
+        //         password
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(data => {
+        //     console.log(data)
+        //     Navigate('/signin')
+        // })
+    }
 
     return (
         <div>
@@ -45,7 +68,8 @@ export default function Signup() {
                 </Grid>
                 <Grid item
                     xs={12}>
-                    <Button variant="contained" color="primary">
+                    <Button variant="contained" color="primary"
+                        onClick={handleClick}>
                         Sign Up
                     </Button>
                 </Grid>
